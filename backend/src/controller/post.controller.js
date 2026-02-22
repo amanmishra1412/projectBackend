7aa6cdc930eea5d0ddfa4310271c0164e4d47ca7
amanmishra1412 <mishra06aman@gmail.com>
2026-02-23 00:50:22 +0530

    feature: post fetch

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -21,6 +21,7 @@ const createPost = async (req, res) => {
     } catch (err) {
         return res.status(401).json({ msg: "User Not Authorized" });
     }
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), "file"),
         fileName: "Test",
@@ -36,4 +37,58 @@ const createPost = async (req, res) => {
     res.status(200).json({ msg: "Post Create success", post });
 };
 
-module.exports = { createPost };
+const getPost = async (req, res) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ msg: "Token not provided" });
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({ msg: "User Not Authorized" });
+    }
+
+    const userId = decoded.id;
+    const posts = await postModel.find({ user: userId });
+
+    if (!posts) {
+        return res.status(404).json({ msg: "Not Found" });
+    }
+
+    return res.status(200).json({ msg: "Post Fetched", posts });
+};
+
+const getDetailPost = async (req, res) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ msg: "Token not provided" });
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({ msg: "User Not Authorized" });
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.id;
+
+    const posts = postModel.findOne({ postId });
+
+    const validUser = posts.user.toString() === userId;
+
+    if (validUser) {
+        return res.status(403).json({ msg: "Unauthorized" });
+    }
+
+    return res.status(200).json({ msg: "Fetched Success", posts });
+};
+
+module.exports = { createPost, getPost, getDetailPost };
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const postRouter = express.Router();
-const { createPost } = require("../controller/post.controller");
+const {
+    createPost,
+    getPost,
+    getDetailPost,
+} = require("../controller/post.controller");
+
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 postRouter.post("/", upload.single("image"), createPost);
+postRouter.get("/", getPost);
+postRouter.get("/details/:postId", getDetailPost);
 
 module.exports = postRouter;
